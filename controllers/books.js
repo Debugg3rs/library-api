811d5b6c4dbf21bdf8d1fe242d0cf3052318f50a
anangben <anangben932@gmail.com>
2025-03-10 16:08:09 +0000

    Update book controller removed

--- controllers/books.js
+++ controllers/books.js
@@ -1,38 +1,20 @@
-
 //Add book controller
-export const addBooks = async(req, res, next) => {
-    try {
-        const book = await Book.create(req.body)
-        await book.save()
-        res.status(201).json({message: "Book added successfully"})
-    } catch (error) {
-        res.status(400).json({message: error.message})
-    }
-    }
-
+export const addBooks = async (req, res, next) => {
+  try {
+    const book = await Book.create(req.body);
+    await book.save();
+    res.status(201).json({ message: "Book added successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 //Fetch book controller
-export const getBooks = async(req, res, next) => {
-    try {
-        const book = await Book.find(req.body)
-        res.status(200).json({message: "Book fetched successfully"})
-    } catch (error) {
-       res.status(304).json({message: error.message})
-    }
-    }
-
-
-
-
-
-
-
-export const updateBook = async(req, res, next) =>{
-    try {
-        const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true})
-        res.status(200).json(message: "Book updated successfully")
-    } catch (error) {
-        res.status(204).json({message: error.message})
-        
-    }
-}
+export const getBooks = async (req, res, next) => {
+  try {
+    const book = await Book.find(req.body);
+    res.status(200).json({ message: "Book fetched successfully" });
+  } catch (error) {
+    res.status(304).json({ message: error.message });
+  }
+};
