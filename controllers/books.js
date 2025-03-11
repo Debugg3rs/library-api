93fa7522043ab336930793a04c86725837ea355f
gloria larbi <Miss-Gloria>
2025-03-11 14:03:35 +0000

    worked on my controlller.js

--- controllers/books.js
+++ controllers/books.js
@@ -1,16 +1,20 @@
-export const updateBook = async (req, res, next) => {
+import { Book } from "../models/book.js";
+
+//Update the updateBook
+export const updateBook = async (req, res) => {
   try {
       const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true })
-      res.status(200).json(book)('message', 'Book updated successfully')
+      res.status(200).json(book)({ message: "Book updated successfully", book: book })
   } catch (error) {
     res.status(204).json({ message: error.message })
   }
 };
- 
-export const deleteBook = async (req, res, next) => {
+
+//Update the deleteBook(controller)
+export const deleteBook = async (req, res) => {
   try {
-    await Book.findByIdAndDelete(req.params.id)
-    res.status(200).json({ message: 'Book deleted successfully' })
+   const book =  await Book.findByIdAndDelete(req.params.id)
+    res.status(200).json({ message: "Book updated successfully", book: book })
   } catch (error) {
     res.status(204).json({ message: error.message })
   }
