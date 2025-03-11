deee577940cafc294b20e060dab8b5ed172f1864
unknown <soketessuman@gmail.com>
2025-03-11 08:40:36 +0000

    modified the server

---
+++ index.js
@@ -0,0 +1,23 @@
+import config from "./utils/config.js";
+import express from "express";
+import { connect } from "mongoose";
+
+
+const app = express();
+
+// Connect to MongoDB
+console.log("Connecting to MongoDB");
+connect(config.MONGODB_URI, {
+})
+ .then(() => console.log("Connected to MongoDB"))
+
+
+// Middleware for parsing JSON request bodies
+app.use(express.json());
+
+
+
+app.listen(config.PORT, () => {
+  console.log(`Server is listening on port ${config.PORT}`);
+
+});
